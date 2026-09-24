add2037eb1318ad17276e7ba03f9c5bb0056f47e
Min RK <benjaminrk@gmail.com>
2026-09-24 10:51:44 -0700

    open-wearables almost works

--- ci/helm_install.sh
+++ ci/helm_install.sh
@@ -5,7 +5,7 @@ set -ex
 ns="jhe-test"
 name="jhe-test"
 kubectl create ns $ns || true
-helm upgrade --install --namespace $ns $name ./jhe --values tests/test_values.yaml
+helm upgrade --install --namespace $ns $name ./jhe --values tests/test_values.yaml --skip-schema-validation
 
 kubectl rollout -n jhe-test status deployments/$name-postgres --timeout=60s
 kubectl rollout -n jhe-test status deployments/$name --timeout=60s
